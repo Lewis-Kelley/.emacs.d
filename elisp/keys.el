68be818b5e859ee9d389444b07db5375a13be9a0
Lewis Kelley <kelleyld@rose-hulman.edu>
2015-10-05 07:53:39 -0400

    Added more packages and reorganized some binds.

--- elisp/keys.el
+++ elisp/keys.el
@@ -9,17 +9,11 @@
 (global-set-key (kbd "s-e") 'event)
 (global-set-key (kbd "s-n") 'notes)
 (global-set-key (kbd "s-v") 'view-notes)
-(global-set-key (kbd "s-c") 'calc)
-(global-set-key (kbd "s-t") 'multi-term)
 
 ;; custom desktop save/load commands
 (global-set-key (kbd "C-c d") 'my-desktop-save)
 (global-set-key (kbd "C-c r") 'desktop-read)
 (global-set-key (kbd "C-x c") 'desktop-save-and-close)
-
-;; magit
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 ;; dealing with other frames
 (global-set-key (kbd "C-c o") 'other-frame)
@@ -29,8 +23,11 @@
 (global-set-key (kbd "C-c 1") 'delete-other-frames)
 
 ;; miscellaneous
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-x l") 'ispell-buffer)
+(global-set-key (kbd "s-f") 'hs-toggle-hiding)
+(global-set-key (kbd "<f5>") (lambda ()
+                               (interactive)
+                               (setq-local compilation-read-command nil)
+                               (call-interactively 'compile)))
 
 (provide 'keys)
 
--- elisp/packages.el
+++ elisp/packages.el
@@ -11,7 +11,12 @@
   :init
   (add-hook 'c-mode-hook 'flycheck-mode)
   (add-hook 'c-mode-hook #'yas-minor-mode)
+  (add-hook 'c-mode-common-hook 'hs-minor-mode)
   (setq-default c-basic-offset 4))
+
+(use-package calc
+  :init
+  (global-set-key (kbd "s-c") 'calc))
 
 (use-package cdlatex
   :ensure t)
@@ -34,6 +39,17 @@
   (add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
   (add-hook 'emacs-lisp-mode-hook #'yas-minor-mode))
 
+(use-package flx-ido
+  :ensure t
+  :init
+  (setq gc-cons-threshold 20000000) ;; up the emacs garbage collection threshold
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (flx-ido-mode 1)
+  ;; disable ido faces to see flx highlights.
+  (setq ido-enable-flex-matching t)
+  (setq ido-use-faces nil))
+
 (use-package flycheck
   :ensure t
   :init
@@ -46,13 +62,45 @@
   :init
   (fa-config-default))
 
+(use-package gdb
+  :init
+  (setq gdb-many-windows t)
+  (setq gdb-show-main t))
+
+(use-package ggtags
+  :ensure t
+  :init
+  (add-hook 'c-mode-common-hook
+	    (lambda ()
+	      (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+		(ggtags-mode 1))))
+
+  (define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+  (define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+  (define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+  (define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+  (define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+  (define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+
+  (define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark))
+
 (use-package gnuplot
   :ensure t)
+
+(use-package ido
+  :init
+  (ido-mode t))
+
+(use-package ispell
+  :init
+  (global-set-key (kbd "C-x l") 'ispell-buffer))
 
 (use-package magit
   :ensure t
   :init
   (setq magit-restore-window-configuration t)
+  (global-set-key (kbd "C-x g") 'magit-status)
+  (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
   (global-magit-file-mode))
 
 (use-package monokai-theme
@@ -65,10 +113,17 @@
   :init
   (multicolumn-global-mode 1))
 
+(use-package multiple-cursors
+  :ensure t
+  :init
+  (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines))
+
 (use-package multi-term
   :ensure t
   :init
+  (global-set-key (kbd "s-t") 'multi-term)
   (setq multi-term-program "/bin/bash"))
+
 (use-package org-mode
    :init
    (setq org-startup-indented t)
@@ -76,13 +131,20 @@
    (setq org-agenda-start-on-weekday nil)
    (add-hook 'org-mode-hook 'turn-on-org-cdlatex)
    (add-hook 'org-mode-hook 'org-preview-latex-fragment)
+   (global-set-key (kbd "C-c a") 'org-agenda)
    (add-hook 'org-mode-hook
- 	    (lambda () (local-set-key (kbd "C-c C-x M-l") (kbd "C-u C-u C-c C-x C-l")))))
+	     (lambda () (local-set-key (kbd "C-c C-x M-l") (kbd "C-u C-u C-c C-x C-l")))))
 
 (use-package powerline-evil
   :ensure t
   :init
   (powerline-evil-center-color-theme))
+
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-global-mode)
+  (setq projectile-enable-caching t))
 
 (use-package seethru
   :ensure t
