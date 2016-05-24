5ad494f468a27c0a079811ec03a8d30756b19136
Lewis-Kelley <kelleyld@rose-hulman.edu>
2016-05-24 08:58:57 +0000

    Added smart-mode-line and cleaned up some more.

--- elisp/keys.el
+++ elisp/keys.el
@@ -33,14 +33,11 @@
 (global-set-key (kbd "C-x )") 'delete-window)
 
 ;; strengthen escape
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
 (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-(global-set-key [escape] 'evil-exit-emacs-state)
 
 (provide 'keys)
 
