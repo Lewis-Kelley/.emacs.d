669991c1150f08afe9a9b38b8f2d21a18742b3d2
Lewis-Kelley <kelleyld@rose-hulman.edu>
2016-12-11 12:07:50 -0500

    Lots of org stuff as well as changning tab width.

--- yam-mode.el
+++ yam-mode.el
@@ -17,9 +17,8 @@
 (defvar yam-cursor-color (list "blue")
   "A list of the colors used for each state.")
 
-(defvar yam-states-key (make-hash-table :test 'equal)
-  "Hashtable of all the different created states to keep track of their index.")
-(puthash "Normal" 0 yam-states-key)
+(defvar yam-states-key (list '(Normal . 0))
+  "List of conses of all the different created states and their index.")
 
 (defvar yam-curr-state 0
   "The index of the currently selected state.")
@@ -32,42 +31,54 @@
   (setq yam-maps (list (make-sparse-keymap)))
   (setq yam-cursor-type (list t))
   (setq yam-cursor-color (list "blue"))
-  (setq yam-states-key (make-hash-table :test 'equal))
-  (puthash "Normal" 0 yam-states-key)
+  (setq yam-states-key (list '(Normal . 0)))
   (setq yam-curr-state 0)
   (setq yam-mode-map (car yam-maps)))
+
+(defun yam-find-index (name)
+  "Return the index associated with the yam state NAME."
+  (let ((index '()))
+    (mapc (lambda (item)
+            (when (equal name (car item))
+              (setq index (cdr item))))
+          yam-states-key)
+    index))
 
 (defun yam-create-state (name)
   "Create a new state with the given NAME."
   (add-to-list 'yam-maps (make-sparse-keymap))
   (add-to-list 'yam-cursor-type t)
   (add-to-list 'yam-cursor-color (list "white"))
-  (puthash name (hash-table-count yam-states-key) yam-states-key))
+  (add-to-list 'yam-states-key (cons name (length yam-states-key))))
 
 (defun yam-add-binding (name key func)
   "Add a new binding to yam state NAME.
 When KEY is pressed in NAME state, run FUNC."
-  (let* ((index (- (hash-table-count yam-states-key)
-                   (gethash name yam-states-key)
-                   1))
-         (yam-map (nth index yam-maps)))
+  (let* ((index (yam-find-index name))
+         (yam-map (nth (- (length yam-maps) index 1) yam-maps)))
     (define-key yam-map (kbd key) func)))
 
 (defun yam-set-state (name)
   "Set the current yam-state to NAME."
-  (interactive "sEnter mode: ")
-  (setq yam-curr-state (gethash name yam-states-key))
-  (setq yam-mode-map (nth (- (hash-table-count yam-states-key) yam-curr-state 1)
-                          yam-maps)))
+  (interactive "SEnter mode: ")
+  (setq yam-curr-state (yam-find-index name))
+  (setq yam-mode-map (nth (- (length yam-maps) yam-curr-state 1) yam-maps)))
+
+(defun yam-get-state ()
+    "Print the name of the current state."
+    (interactive)
+    (print (car (nth (- (length yam-states-key) yam-curr-state 1)
+                     yam-states-key))))
 
 (define-minor-mode yam-mode
   "Toggle `yam-minor-mode'."
   nil " yam" yam-mode-map)
 
-(yam-add-binding "Normal" "n" (lambda () (interactive) (message "Hello YAM!")))
-(yam-create-state "Delete")
-(yam-add-binding "Delete" "k" (lambda () (interactive) (message "Goodbye YAM!")))
-(yam-set-state "Delete")
+;; (yam-add-binding 'Normal "n" (lambda () (interactive) (message "Hello YAM!")))
+;; (yam-create-state 'Delete)
+;; (yam-add-binding 'Delete "k" (lambda () (interactive) (message "Goodbye YAM!")))
+;; (yam-set-state 'Delete)
+;; (setq yam-mode-map (cons (kbd "a") (lambda () (interactive) (message "Apple"))))
 
 (provide 'yam-mode)
 ;;; yam-mode.el ends here
